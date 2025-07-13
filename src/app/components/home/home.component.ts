0cf3aba9c998fe636aafa38060b66b9641899dd1
Bea Tavernaro <94928593+beatavernaro@users.noreply.github.com>
2025-07-12 21:52:09 -0300

    Fix: Resolve scaling issues for GitHub Pages deployment

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,6 @@ import { HeroComponent } from '../hero/hero.component';
 import { AboutComponent } from '../about/about.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ArticlesComponent } from '../articles/articles.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +12,6 @@ import { FooterComponent } from '../footer/footer.component';
     AboutComponent,
     ProjectsComponent,
     ArticlesComponent,
-    FooterComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
